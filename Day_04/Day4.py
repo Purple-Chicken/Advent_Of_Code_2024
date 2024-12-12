1ed6215d0317e58a0c3c4c21349f975f2e2be7a2
Purple-Chicken <purple.chickn@rutgers.edu>
2024-12-12 12:50:26 -0500

    Day 4 Vertical Search

--- Day_04/Day4.py
+++ Day_04/Day4.py
@@ -2,24 +2,28 @@ import re
 def main():
     with open('Day4.txt', 'r') as file:
         og_input = file.read().split("\n")
-    print(og_input)
+    #print(og_input)
+    xword = make_array(og_input)
     search_count = 0
     search_count += horizontal_search(og_input, "XMAS")
     search_count += horizontal_search(og_input, "SAMX")
-    #search_count += vertical_search(og_input, "XMAS")
-    #search_count += vertical_search(og_input, "SAMX")
-    #search_count += forward_diag_search(og_input, "XMAS")
-    #search_count += forward_diag_search(og_input, "SAMX")
-    #search_count += back_diag_search(og_input, "XMAS")
-    #search_count += back_diag_search(og_input, "SAMX")
+    print(f"XMAS appears in the word search {search_count} times horizontally.")
+    search_count += vertical_search(xword, "XMAS")
+    search_count += vertical_search(xword, "SAMX")
+    print(f"XMAS appears in the word search {search_count} times horizontally and vertically.")
+    #search_count += forward_diag_search(xword, "XMAS")
+    #search_count += forward_diag_search(xword, "SAMX")
+    #search_count += back_diag_search(xword, "XMAS")
+    #search_count += back_diag_search(xword, "SAMX")
     print(f"XMAS appears in the word search {search_count} times.")
+def make_array(input_str):
+    return [list(i) for i in input_str]
 
 def horizontal_search(array, word):
     count = 0 #Local function count, not global count
     i = 0
     while i < len(array):
         count += len(re.findall(word, array[i]))
-        print(f"Row {i+1} count: {count}")
         i += 1
     return count
 
@@ -27,9 +31,21 @@ def vertical_search(array,word):
     count = 0 #Local function count, not global count
     i=0
     while i < len(array[0]):
-        print()
-        i+= 1
+        temp_array = []
+        j=0
+        while j < len(array[0]):
+            temp_array.append(array[i][j])
+            count += len(re.findall(word, ''.join(temp_array)))
+            print(count)
+            j += 1
+        i += 1
     return count
+
+def forward_diag_search(array, word):
+    count = 0
+
+    return count
+
 
 
 if __name__ == "__main__":
